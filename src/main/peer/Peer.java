5da35aa5ad5a46889d69603380f037f97f6febd7
Laurence Mullen <laurencemullen99@gmail.com>
2021-09-29 19:38:45 -0400

    base project structure

---
+++ src/main/peer/Peer.java
@@ -0,0 +1,18 @@
+/*
+    Peer Process
+
+    * Takes peerID as parameter.
+    * Reads Common.cfg to set variables
+    * Reads PeerInfo.cfg to find other peers that have file
+    * Set bits of bitfield to 0 or 1 depending on above
+    * Listen on port for other peers if first
+    * Make connections to already started peers
+    * Terminate when all peers have complete file
+
+*/
+
+package main.peer;
+
+public class Peer {
+
+}
