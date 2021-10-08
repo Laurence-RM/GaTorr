6ba57202e820d556ed2d743a8c9aaf4ccd63617a
Laurence Mullen <laurencemullen99@gmail.com>
2021-10-07 20:26:11 -0400

    Added config reading to peer class

--- src/main/peer/Peer.java
+++ src/main/peer/Peer.java
@@ -13,6 +13,67 @@
 
 package main.peer;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Peer {
 
+    private int peerID;
+
+    private int numPreferredNeighbors;
+    private int unchokingInterval;
+    private int optimisticUnchokingInterval;
+    private String fileName;
+    private int fileSize;
+    private int pieceSize;
+
+    public Peer(int id) {
+        this.peerID = id;
+
+        // Read common cfg
+        try {
+            File commonCfg = new File("Common.cfg");
+            Scanner reader = new Scanner(commonCfg);
+            while (reader.hasNextLine()) {
+                String property = reader.next();
+
+                switch (property) {
+                    case "NumberOfPreferredNeighbors":
+                        this.numPreferredNeighbors = reader.nextInt();
+                        break;
+                    case "UnchokingInterval":
+                        this.unchokingInterval = reader.nextInt();
+                        break;
+                    case "OptimisticUnchokingInterval":
+                        this.optimisticUnchokingInterval = reader.nextInt();
+                        break;
+                    case "FileName":
+                        this.fileName = reader.next();
+                        break;
+                    case "FileSize":
+                        this.fileSize = reader.nextInt();
+                        break;
+                    case "PieceSize":
+                        this.pieceSize = reader.nextInt();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            //e.printStackTrace();
+            System.out.println("Common cfg not found.");
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            Peer pTest = new Peer(Integer.parseInt(args[0]));
+        } catch (Exception e) {
+            e.printStackTrace();
+            //System.out.println("Improper args. Missing numerical Peer ID");
+        }
+    }
 }
