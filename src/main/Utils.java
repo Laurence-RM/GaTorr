11f87481cb243e0448b10accad77285ee36f96c0
Laurence Mullen <laurencemullen99@gmail.com>
2021-10-19 03:04:25 -0400

    Added byteArrayToInt and fixed bug

--- src/main/Utils.java
+++ src/main/Utils.java
@@ -11,8 +11,26 @@ public class Utils {
     public static byte[] intToByteArray(int num) {
         byte[] ba = new byte[4];
         for (int i = 3; i >= 0; i--) {
-            ba[i] = (byte) (num >>> i*8);
+            ba[3-i] = (byte) (num >> i*8);
         }
         return ba;
+    }
+
+    public static final int byteArrayToInt(byte[] ba) {
+        if (ba.length != 4) { return -1; }
+
+        int out = 0;
+        for (int i = 3; i >= 0; i--) {
+            out |= ((ba[3-i] & 0xFF) << i*8);
+        }
+        return out;
+    }
+
+    public static void main(String args[]) {
+        byte[] b = intToByteArray(1234);
+        for (byte B : b) {
+            System.out.printf("%02x ", B);
+        }
+        System.out.println("\n"+byteArrayToInt(b));
     }
 }
