6c875cab55f37dcf922185a848966af613653916
Laurence Mullen <laurencemullen99@gmail.com>
2021-10-19 03:03:59 -0400

    Made and tested Handshake msg class

--- src/main/peer/message/Handshake.java
+++ src/main/peer/message/Handshake.java
@@ -9,6 +9,54 @@
 
 package main.peer.message;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import main.Utils;
+
 public class Handshake {
-    
+    private static String HEADER = "P2PFILESHARINGPROJ";
+    private int peerID;
+
+    public Handshake(int ID) {
+        this.peerID = ID;
+    }
+
+    public byte[] getMessage() {
+        ByteArrayOutputStream outBytes = new ByteArrayOutputStream(32);
+        outBytes.write(HEADER.getBytes(), 0, 18);
+        
+        byte[] zeros = new byte[10];
+        Arrays.fill(zeros, (byte) 0);
+        outBytes.write(zeros, 0, 10);
+        outBytes.write(Utils.intToByteArray(peerID), 0, 4);
+
+        return outBytes.toByteArray();
+    }
+
+    public static int validateMessage(byte[] msg) {
+        if (msg.length != 32) { return -1; }
+        ByteArrayInputStream inBytes = new ByteArrayInputStream(msg);
+        byte[] bHeader = new byte[18];
+        inBytes.read(bHeader, 0, 18);
+        if (!new String(bHeader).equals(HEADER)) {
+            return -1;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (inBytes.read() != 0) {
+                return -1;
+            }
+        }
+
+        byte[] id = new byte[4];
+        inBytes.read(id, 0, 4);
+        return Utils.byteArrayToInt(id);
+    }
+
+    public static void main(String args[]) {
+        Handshake hs = new Handshake(1001);
+        int res = validateMessage(hs.getMessage());
+        System.out.println(res);
+    }
 }
